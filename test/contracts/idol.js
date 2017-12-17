6451eccd2800a3bc08ec28e2d1a41329b0cbf13a
shiki-tak <shiki.tak@gmail.com>
2017-12-17 15:03:10 +0900

    add check existence idol information test

--- test/contracts/idol.js
+++ test/contracts/idol.js
@@ -1,15 +1,31 @@
 const IdolContract = artifacts.require('IdolContract.sol');
 
 contract('IdolContract', (accounts) => {
+
+  before (async () => {
+    idolcontract = await IdolContract.new();
+  });
+
   it('should be deployed and set idol for IdolTest', async function () {
     const name = "hirosesuzu";
     const issuance = 5;
 
-    idolcontract = await IdolContract.new();
+    // idolcontract = await IdolContract.new();
     await idolcontract.setIdol(name, issuance);
     const first_idol = await idolcontract.getIdol(0);
 
     assert.strictEqual(first_idol[0], name, 'Not correct set name!');
     assert.strictEqual(first_idol[1].toNumber(), issuance, 'Not correct set issuance!')
+  });
+
+  it('should be correct check idol existence', async function () {
+    const existence_target_id = 0;
+    const notexistence_traget_id = 1;
+
+    const check_result0 = await idolcontract.checkIdolExistence(existence_target_id);
+    assert.strictEqual(check_result0, true, 'Not correct check result');
+
+    const check_result1 = await idolcontract.checkIdolExistence(notexistence_traget_id);
+    assert.strictEqual(check_result1, false, 'Not corrrect result');
   });
 });
