2fd2a2c1a11a53ed21eb876412eea1b1365e583e
荒川幸生 <arakawayukio@arakawasachio-no-MacBook-Air.local>
2017-12-18 23:17:53 +0900

    アイドル取得処理

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+
+  end
+
+  # アイドルくじを引く
+  def draw
+    @idol = Idol.get_idol
+  end
+
+  def my_idols
+
   end
 end
--- app/models/idol.rb
+++ app/models/idol.rb
@@ -1,2 +1,6 @@
 class Idol < ApplicationRecord
+  def self.get_idol
+    # TODO
+    Idol.all.sample
+  end
 end
