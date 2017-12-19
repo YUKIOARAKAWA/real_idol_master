2c891396e5e669bad493cd3148a3113c5ef3502c
荒川幸生 <arakawayukio@arakawasachio-no-MacBook-Air.local>
2017-12-20 00:13:07 +0900

    引いたアイドルをユーザが所有するできるように

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
   # アイドルくじを引く
   def draw
     @idol = Idol.get_idol
+    @draw_idol = DrawIdol.create_from_idol!(@idol, current_user)
   end
 
   def my_idols
